945d6470677190637bf3e63c6861496407b961ff
Kioubit <kioubit@localhost.localdomain>
2022-10-09 18:10:20 +0300

    Make recursion optional, more options

--- README.md
+++ README.md
@@ -3,32 +3,34 @@
 ## Shift UIDs,GIDs of directories or files by a specified amount
 
 ### Features
-* Supports hardlinks, symlinks
-* Preserves file permissions by default (including setuid)
+* Optional recursion mode
+* Supports hardlinks, symlinks without following them
+* Preserves file modes/permissions by default (including setuid)
 
 ### Example
 
     # ls -la
-    total 8
     drwxr-xr-x 2 user user 4096 Oct  9 15:08 .
     drwxr-xr-x 3 user user 4096 Oct  9 15:16 ..
     -rw-r--r-- 1 2000 2000    0 Oct  9 15:08 testfile
-    drwxr-xr-x 1 2000 2000    0 Oct  9 15:08 testdirectory
-    # chownshift -1 . # Shift UIDs and GIDs by -1
+    drwxr-xr-x 2 2000 2000 4096 Oct  9 15:08 testdirectory
+    # chownshift -1 . --recursive # Shift UIDs and GIDs by -1
     # ls -la
-    total 8
     drwxr-xr-x 2 user user 4096 Oct  9 15:08 .
     drwxr-xr-x 3 user user 4096 Oct  9 15:16 ..
     -rw-r--r-- 1 1999 1999    0 Oct  9 15:08 testfile
-    drwxr-xr-x 1 1999 1999    0 Oct  9 15:08 testdirectory
+    drwxr-xr-x 2 1999 1999 4096 Oct  9 15:08 testdirectory
 
 ### Usage
-    Usage: ./chownshift <difference> <path> [Optional Arguments]
+
+    Usage: chownshift <difference> <path> [Optional Arguments]
     Mandatory Arguments:
     <difference>: A positive or negative integer denoting the amount of UIDs and GIDs the files in path are to be shifted by
     <path>: The target path to a file or directory
     Optional Arguments:
+    --recursive Recurse though the path provided
     --verbose Verbose output
     --nopermissions Do not preserve permissions
+    --dry-run Simulate only
     --help Show this message
     Exit codes: 0 - Success, 1 - Error, 2 - Argument error
--- main.go
+++ main.go
@@ -13,7 +13,9 @@ import (
 )
 
 var verbose = false
-var nopermissions = false
+var noPermissions = false
+var recursive = false
+var dryRun = false
 
 func main() {
 
@@ -30,8 +32,13 @@ func main() {
 			switch arg {
 			case "verbose":
 				verbose = true
+			case "recursive":
+				recursive = true
 			case "nopermissions":
-				nopermissions = true
+				noPermissions = true
+			case "dry-run":
+				dryRun = true
+				verbose = true
 			default:
 				printUsage()
 				return
@@ -48,6 +55,8 @@ func main() {
 	// -----------------------------------------------
 
 	target := cmdArgs[2]
+
+	var err error
 	difference, err := strconv.Atoi(cmdArgs[1])
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error: Invalid integer for difference value")
@@ -56,88 +65,123 @@ func main() {
 	}
 
 	verboseLog("Simulating...")
-	err = apply(target, difference, true)
+	if recursive {
+		err = applyRecursively(target, difference, true)
+	} else {
+		knownInodes := make([]uint64, 0)
+		var info os.FileInfo
+		info, err = os.Lstat(target)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error: "+err.Error())
+			os.Exit(1)
+		}
+		err = applyToFile(&knownInodes, target, info, difference, true)
+	}
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "Error: "+err.Error())
 		os.Exit(1)
 	}
 
-	verboseLog("Applying...")
-	err = apply(target, difference, false)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "Error: "+err.Error())
-		os.Exit(1)
+	if !dryRun {
+		verboseLog("Applying...")
+		if recursive {
+			err = applyRecursively(target, difference, false)
+		} else {
+			knownInodes := make([]uint64, 0)
+			var info os.FileInfo
+			info, err = os.Lstat(target)
+			if err != nil {
+				fmt.Fprintln(os.Stderr, "Error: "+err.Error())
+				os.Exit(1)
+			}
+			err = applyToFile(&knownInodes, target, info, difference, false)
+		}
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "Error: "+err.Error())
+			os.Exit(1)
+		}
 	}
 }
 
-func apply(target string, difference int, simulate bool) error {
-	knownInodes := []uint64{}
+func applyRecursively(target string, difference int, simulate bool) error {
+	knownInodes := make([]uint64, 0)
 	return filepath.Walk(target, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if path == "." {
+		if path == "." && target != "." {
 			return nil
 		}
 
-		stat := info.Sys().(*syscall.Stat_t)
-		if stat == nil {
-			return fmt.Errorf("could not perform stat on %s", path)
-		}
-
-		nlink := stat.Nlink
-		inode := stat.Ino
-		if nlink > 1 {
-			found := false
-			for _, b := range knownInodes {
-				if b == inode {
-					found = true
-					break
-				}
-			}
-			if !found {
-				knownInodes = append(knownInodes, inode)
-			} else {
-				return nil
-			}
-		}
-
-		startgid := stat.Gid
-		startuid := stat.Uid
-
-		rawgid := int64(startgid) + int64(difference)
-		rawuid := int64(startuid) + int64(difference)
-		if rawgid < 0 || rawuid < 0 {
-			return fmt.Errorf("%s: trying to assing uid or gid less than zero. (%d,%d -> %d,%d)", path, startuid, startgid, rawuid, rawgid)
-		}
-
-		var targetgid, targetuid uint32
-		targetgid = uint32(rawgid)
-		targetuid = uint32(rawuid)
-
-		if !simulate {
-			verboseLog(fmt.Sprintf("%s: %d,%d -> %d,%d", path, startuid, startgid, targetuid, targetgid))
-			err = os.Lchown(path, int(targetuid), int(targetgid))
-			if err != nil {
-				return fmt.Errorf("chown %s: %s", path, err.Error())
-			}
-
-			if !nopermissions {
-				//Restore permissions (important for setuid that gets lost after chown)
-				if info.Mode()&os.ModeSymlink != os.ModeSymlink {
-					//In case this is not a symlink (Cannot set permissions on symlink under linux)
-					err := unix.Fchmodat(unix.AT_FDCWD, path, stat.Mode, 0)
-					if err != nil {
-						err = &os.PathError{Op: "chmod", Path: path, Err: err}
-						return err
-					}
-				}
-			}
-
-		}
-		return nil
+		return applyToFile(&knownInodes, path, info, difference, simulate)
 	})
+}
+
+func applyToFile(knownInodes *[]uint64, path string, info os.FileInfo, difference int, simulate bool) error {
+	stat := info.Sys().(*syscall.Stat_t)
+	if stat == nil {
+		return fmt.Errorf("could not perform stat on %s", path)
+	}
+
+	nlink := stat.Nlink
+	inode := stat.Ino
+	if nlink > 1 {
+		found := false
+		for _, b := range *knownInodes {
+			if b == inode {
+				found = true
+				break
+			}
+		}
+		if !found {
+			*knownInodes = append(*knownInodes, inode)
+		} else {
+			verboseLog(fmt.Sprintf("[Simulating] %s: Skip hard link ", path))
+			return nil
+		}
+	}
+
+	startgid := stat.Gid
+	startuid := stat.Uid
+
+	rawgid := int64(startgid) + int64(difference)
+	rawuid := int64(startuid) + int64(difference)
+	if rawgid < 0 || rawuid < 0 {
+		return fmt.Errorf("%s: trying to assing uid or gid less than zero. (%d,%d -> %d,%d)", path, startuid, startgid, rawuid, rawgid)
+	}
+
+	var targetgid, targetuid uint32
+	targetgid = uint32(rawgid)
+	targetuid = uint32(rawuid)
+
+	if simulate {
+		verboseLog(fmt.Sprintf("[Simulating] %s: %d,%d -> %d,%d", path, startuid, startgid, targetuid, targetgid))
+	} else {
+		verboseLog(fmt.Sprintf("[Changing] %s: %d,%d -> %d,%d", path, startuid, startgid, targetuid, targetgid))
+	}
+
+	if !simulate {
+		err := os.Lchown(path, int(targetuid), int(targetgid))
+		if err != nil {
+			return fmt.Errorf("chown %s: %s", path, err.Error())
+		}
+
+		if !noPermissions {
+			//Restore permissions (important for setuid that gets lost after chown)
+			if info.Mode()&os.ModeSymlink != os.ModeSymlink {
+				//In case this is not a symlink (Cannot set permissions on symlink under linux)
+				err := unix.Fchmodat(unix.AT_FDCWD, path, stat.Mode, 0)
+				if err != nil {
+					err = &os.PathError{Op: "chmod", Path: path, Err: err}
+					return err
+				}
+			}
+		}
+
+	}
+	return nil
+
 }
 
 func verboseLog(log string) {
@@ -152,8 +196,10 @@ func printUsage() {
 	fmt.Println("<difference>: A positive or negative integer denoting the amount of UIDs and GIDs the files in path are to be shifted by")
 	fmt.Println("<path>: The target path to a file or directory")
 	fmt.Println("Optional Arguments:")
+	fmt.Println("--recursive Recurse though the path provided")
 	fmt.Println("--verbose Verbose output")
 	fmt.Println("--nopermissions Do not preserve permissions")
+	fmt.Println("--dry-run Simulate only")
 	fmt.Println("--help Show this message")
 	fmt.Println("Exit codes: 0 - Success, 1 - Error, 2 - Argument error")
 }
